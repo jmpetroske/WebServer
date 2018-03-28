6b7579e25b8fc8b37061fdd628cace1435868f36
Joseph Petroske <jmpetroske@gmail.com>
2018-03-28 13:15:33 -0700

    Simple add row to database

--- src/server/main.go
+++ src/server/main.go
@@ -4,6 +4,9 @@ import (
 	"net/http"
 	"strings"
 	"fmt"
+    // "golang.org/x/crypto/bcrypt"
+	"database/sql"
+	_ "github.com/lib/pq"
 )
 
 func sayHello(w http.ResponseWriter, r *http.Request) {
@@ -15,7 +18,24 @@ func sayHello(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	connString := "postgres://petroske:@localhost/accounts?sslmode=disable"
+	
+	db, err := sql.Open("postgres", connString)
+	checkErr(err)
+	
+	_, err = db.Query(
+		`INSERT INTO user_account (username, password_hash) VALUES ($1, $2);`,
+		"jmpetroske",
+		"password")
+	checkErr(err)
+
 	http.HandleFunc("/", sayHello)
 	panic(http.ListenAndServe(":8080", nil))
 	fmt.Print("test")
+}
+
+func checkErr(err error) {
+	if err != nil {
+		panic(err)
+	}
 }
