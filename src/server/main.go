fb856f6fa2cbccdf96adb7e04bdae63afdbece78
Joseph Petroske <jmpetroske@gmail.com>
2018-03-28 20:29:23 -0700

    Adding support for login and create account pages

--- src/server/main.go
+++ src/server/main.go
@@ -3,10 +3,10 @@ package main
 import (
 	"net/http"
 	"strings"
-	"fmt"
-    // "golang.org/x/crypto/bcrypt"
+//    "golang.org/x/crypto/bcrypt"
 	"database/sql"
 	_ "github.com/lib/pq"
+	"github.com/gorilla/mux"
 )
 
 func sayHello(w http.ResponseWriter, r *http.Request) {
@@ -17,21 +17,57 @@ func sayHello(w http.ResponseWriter, r *http.Request) {
 	w.Write([] byte(message))
 }
 
-func main() {
-	connString := "postgres://petroske:@localhost/accounts?sslmode=disable"
-	
-	db, err := sql.Open("postgres", connString)
-	checkErr(err)
-	
-	_, err = db.Query(
-		`INSERT INTO user_account (username, password_hash) VALUES ($1, $2);`,
-		"jmpetroske",
-		"password")
-	checkErr(err)
+func loginHandler(w http.ResponseWriter, r *http.Request) {
+	username := r.FormValue("usr")
+	password := r.FormValue("pwd")
 
-	http.HandleFunc("/", sayHello)
-	panic(http.ListenAndServe(":8080", nil))
-	fmt.Print("test")
+	if username != "" && password != "" {
+
+		
+		databaseConnString := "postgres://petroske:@localhost/accounts?sslmode=disable"
+		
+		db, err := sql.Open("postgres", databaseConnString)
+		checkErr(err)
+		
+		_, err = db.Query(
+			`INSERT INTO user_account (username, password_hash) VALUES ($1, $2);`,
+			username,
+			"password")
+		checkErr(err)
+
+		//err := bcrypt.CompareHashAndPassword
+	}
+}
+
+func createAccountHandler(w http.ResponseWriter, r *http.Request) {	
+	username := r.FormValue("usr")
+	password := r.FormValue("pwd")
+
+//	redirectTarget := "/"
+	if username != "" && password != "" {
+		databaseConnString := "postgres://petroske:@localhost/accounts?sslmode=disable"
+		
+		db, err := sql.Open("postgres", databaseConnString)
+		checkErr(err)
+		
+		_, err = db.Query(
+			`INSERT INTO user_account (username, password_hash) VALUES ($1, $2);`,
+			username,
+			"password")
+		checkErr(err)
+
+	}
+}
+
+func main() {
+	router := mux.NewRouter()
+	router.HandleFunc("/hello", sayHello)
+	router.HandleFunc("/login", loginHandler).Methods("POST")
+	router.HandleFunc("/createAccount", createAccountHandler).Methods("POST")
+	router.PathPrefix("/").Handler(http.FileServer(http.Dir("./static/")))
+	http.Handle("/", router)
+	
+	panic(http.ListenAndServe(":8080", router))
 }
 
 func checkErr(err error) {
